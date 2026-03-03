4eedbd564b0cf427e19119027a76a37faef5c7fb
gente-tech <gente.tech.co@gmail.com>
2026-03-03 09:42:43 -0500

    Create SliderBlock.php

---
+++ web/modules/custom/dermau_core/src/Plugin/Block/SliderBlock.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\dermau_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\node\Entity\Node;
+
+/**
+ * Provides a Slider Block.
+ *
+ * @Block(
+ *   id = "dermau_slider_block",
+ *   admin_label = @Translation("Dermau Slider Block"),
+ * )
+ */
+class SliderBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $query = \Drupal::entityQuery('node')
+      ->condition('type', 'slider')
+      ->condition('status', 1)
+      ->sort('changed', 'DESC')
+      ->accessCheck(TRUE);
+
+    $nids = $query->execute();
+
+    $sliders = [];
+
+    if (!empty($nids)) {
+      $nodes = Node::loadMultiple($nids);
+
+      foreach ($nodes as $node) {
+
+        $sliders[] = [
+          'titulo_azul' => $node->get('field_titulo_azul')->value ?? '',
+          'titulo_blanco' => $node->get('field_titulo_blanco')->value ?? '',
+          'items_texto' => array_column($node->get('field_item_texto')->getValue(), 'value'),
+          'texto_cta' => $node->get('field_texto_call_to_action')->value ?? '',
+          'url_cta' => !$node->get('field_url_call_to_action')->isEmpty()
+            ? $node->get('field_url_call_to_action')->first()->getUrl()->toString()
+            : '',
+        ];
+      }
+    }
+
+    return [
+      '#theme' => 'dermau_slider_block',
+      '#sliders' => $sliders,
+      '#cache' => [
+        'tags' => ['node_list:slider'],
+      ],
+    ];
+  }
+
+}
