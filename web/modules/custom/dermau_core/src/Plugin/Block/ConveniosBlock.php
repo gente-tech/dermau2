0989ca685dfc74a3bfc1b61bddf700169ef62a30
gente-tech <gente.tech.co@gmail.com>
2026-03-03 21:45:47 -0500

    Update ConveniosBlock.php

--- web/modules/custom/dermau_core/src/Plugin/Block/ConveniosBlock.php
+++ web/modules/custom/dermau_core/src/Plugin/Block/ConveniosBlock.php
@@ -3,16 +3,69 @@
 namespace Drupal\dermau_core\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\node\Entity\Node;
 
 /**
  * @Block(
  *   id = "dermau_convenios_block",
- *   admin_label = @Translation("DermaU - Convenios Universitarios (desde contenido)")
+ *   admin_label = @Translation("DermaU - Convenios Universitarios")
  * )
  */
 class ConveniosBlock extends BlockBase {
 
+  /**
+   * Default configuration.
+   */
+  public function defaultConfiguration() {
+    return [
+      'titulo_parte_1' => 'Convenios',
+      'titulo_parte_2' => 'Universitarios',
+      'descripcion' => '',
+    ];
+  }
+
+  /**
+   * Block configuration form.
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+
+    $form['titulo_parte_1'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Título resaltado'),
+      '#default_value' => $this->configuration['titulo_parte_1'],
+      '#required' => TRUE,
+    ];
+
+    $form['titulo_parte_2'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Título normal'),
+      '#default_value' => $this->configuration['titulo_parte_2'],
+      '#required' => TRUE,
+    ];
+
+    $form['descripcion'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Descripción'),
+      '#default_value' => $this->configuration['descripcion'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * Save block configuration.
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+
+    $this->configuration['titulo_parte_1'] = $form_state->getValue('titulo_parte_1');
+    $this->configuration['titulo_parte_2'] = $form_state->getValue('titulo_parte_2');
+    $this->configuration['descripcion'] = $form_state->getValue('descripcion');
+  }
+
+  /**
+   * Build block output.
+   */
   public function build() {
 
     $query = \Drupal::entityQuery('node')
@@ -28,7 +81,6 @@ class ConveniosBlock extends BlockBase {
     if (!empty($nids)) {
 
       $nodes = Node::loadMultiple($nids);
-
       $file_url_generator = \Drupal::service('file_url_generator');
 
       foreach ($nodes as $node) {
@@ -56,6 +108,9 @@ class ConveniosBlock extends BlockBase {
 
     return [
       '#theme' => 'block_convenios',
+      '#titulo_parte_1' => $this->configuration['titulo_parte_1'],
+      '#titulo_parte_2' => $this->configuration['titulo_parte_2'],
+      '#descripcion' => $this->configuration['descripcion'],
       '#items' => $items,
       '#cache' => [
         'tags' => ['node_list'],
