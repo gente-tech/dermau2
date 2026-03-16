f6e00ccffa0eadfd298c8d972dc5cc62e526bab1
Virgilio Padilla Ruiz <vpadillar8@gmail.com>
2026-03-16 14:12:21 -0500

    Bloque info

--- web/modules/custom/dermau_core/src/Plugin/Block/InfoBlock.php
+++ web/modules/custom/dermau_core/src/Plugin/Block/InfoBlock.php
@@ -37,28 +37,25 @@ class InfoBlock extends BlockBase {
       '#type' => 'textfield',
       '#title' => $this->t('Título normal'),
       '#default_value' => $this->configuration['titulo_normal'] ?? '',
-      '#required' => FALSE,
     ];
 
     $form['titulo_resaltado'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Título resaltado'),
       '#default_value' => $this->configuration['titulo_resaltado'] ?? '',
-      '#required' => FALSE,
     ];
 
     $form['descripcion'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Descripción'),
       '#default_value' => $this->configuration['descripcion'] ?? '',
-      '#required' => FALSE,
     ];
 
     $form['imagen'] = [
       '#type' => 'managed_file',
       '#title' => $this->t('Imagen'),
       '#upload_location' => 'public://info-block/',
-      '#default_value' => $this->configuration['imagen'] ?? [],
+      '#default_value' => !empty($this->configuration['imagen']) ? $this->configuration['imagen'] : NULL,
       '#upload_validators' => [
         'file_validate_extensions' => ['png jpg jpeg webp svg'],
       ],
@@ -69,7 +66,6 @@ class InfoBlock extends BlockBase {
       '#type' => 'textfield',
       '#title' => $this->t('Alt de la imagen'),
       '#default_value' => $this->configuration['imagen_alt'] ?? '',
-      '#required' => FALSE,
     ];
 
     return $form;
@@ -81,8 +77,13 @@ class InfoBlock extends BlockBase {
   public function blockSubmit($form, FormStateInterface $form_state) {
     $imagen = $form_state->getValue('imagen');
 
+    if (empty($imagen) && !empty($this->configuration['imagen'])) {
+      $imagen = $this->configuration['imagen'];
+    }
+
     if (!empty($imagen[0])) {
       $file = File::load($imagen[0]);
+
       if ($file) {
         $file->setPermanent();
         $file->save();
@@ -92,7 +93,7 @@ class InfoBlock extends BlockBase {
     $this->configuration['titulo_normal'] = $form_state->getValue('titulo_normal');
     $this->configuration['titulo_resaltado'] = $form_state->getValue('titulo_resaltado');
     $this->configuration['descripcion'] = $form_state->getValue('descripcion');
-    $this->configuration['imagen'] = $imagen;
+    $this->configuration['imagen'] = $imagen ?: [];
     $this->configuration['imagen_alt'] = $form_state->getValue('imagen_alt');
   }
 
@@ -101,9 +102,11 @@ class InfoBlock extends BlockBase {
    */
   public function build() {
     $image_url = '';
+    $image_alt = $this->configuration['imagen_alt'] ?? '';
 
     if (!empty($this->configuration['imagen'][0])) {
       $file = File::load($this->configuration['imagen'][0]);
+
       if ($file) {
         $image_url = \Drupal::service('file_url_generator')
           ->generateAbsoluteString($file->getFileUri());
@@ -117,7 +120,7 @@ class InfoBlock extends BlockBase {
       '#descripcion' => $this->configuration['descripcion'] ?? '',
       '#imagen' => [
         'url' => $image_url,
-        'alt' => $this->configuration['imagen_alt'] ?? '',
+        'alt' => $image_alt,
       ],
       '#cache' => [
         'max-age' => 0,
