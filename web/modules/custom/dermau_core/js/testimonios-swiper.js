7ac4fba5a9a0b9f2ffc4d788ef72fc897d111538
Virgilio Padilla Ruiz <vpadillar8@gmail.com>
2026-04-15 16:12:34 -0500

    RETORNANDO - Agregando bloque de testimonios en interna de programa, ajustando que solo se vea 1 a la vez

--- web/modules/custom/dermau_core/js/testimonios-swiper.js
+++ web/modules/custom/dermau_core/js/testimonios-swiper.js
@@ -1,36 +1,25 @@
-(function (Drupal, once) {
+(function (Drupal) {
   Drupal.behaviors.testimoniosSwiper = {
     attach: function (context) {
-      once('testimoniosSwiper', '.du-testimonials__swiper', context).forEach(function (swiperElement) {
-        new Swiper(swiperElement, {
-          loop: true,
-          slidesPerView: 1,
-          slidesPerGroup: 1,
-          spaceBetween: 0,
-          navigation: {
-            nextEl: swiperElement.closest('.du-testimonials__slider-wrapper').querySelector('.du-testimonials__next'),
-            prevEl: swiperElement.closest('.du-testimonials__slider-wrapper').querySelector('.du-testimonials__prev'),
-          },
-          pagination: {
-            el: swiperElement.closest('.du-testimonials__slider-wrapper').querySelector('.du-testimonials__pagination'),
-            clickable: true,
-          },
-          breakpoints: {
-            0: {
-              slidesPerView: 1,
-              slidesPerGroup: 1,
-            },
-            768: {
-              slidesPerView: 1,
-              slidesPerGroup: 1,
-            },
-            1024: {
-              slidesPerView: 1,
-              slidesPerGroup: 1,
-            }
-          }
-        });
+
+      const swiperElement = context.querySelector('.du-testimonials__swiper');
+      if (!swiperElement || swiperElement.classList.contains('swiper-initialized')) {
+        return;
+      }
+
+      new Swiper('.du-testimonials__swiper', {
+        loop: true,
+        slidesPerView: 1,
+        navigation: {
+          nextEl: '.du-testimonials__next',
+          prevEl: '.du-testimonials__prev',
+        },
+        pagination: {
+          el: '.du-testimonials__pagination',
+          clickable: true,
+        },
       });
+
     }
   };
-})(Drupal, once);
+})(Drupal);
